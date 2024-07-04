8c0578a8a6ecf4c9de6f81df8179753723d7f772
Julio Kozarewicz <julioerk@outlook.com>
2024-07-04 13:10:11 -0300

    [ AUTH MODULE ] ( sanitize ) Init sanitize inputs.

--- src/modules/accounts/accounts.controller.ts
+++ src/modules/accounts/accounts.controller.ts
@@ -2,13 +2,14 @@ import { Controller, Post, Body, ValidationPipe } from '@nestjs/common';
 import { UserEntityDTO } from './accounts.dto';
 import { UserEntity } from './accounts.entity';
 import { UserService } from './accounts.service';
-import { ApiQuery } from '@nestjs/swagger';
+import { ApiQuery, ApiTags } from '@nestjs/swagger';
 
 
 @Controller('accounts')
 export class UserController {
     constructor(private readonly userService: UserService) {}
 
+    @ApiTags('ACCOUNTS')
     @ApiQuery({ type: UserEntityDTO})
     @Post('signup')
     async createUser(@Body(new ValidationPipe({ transform: true })) createUserDto: UserEntityDTO): Promise<UserEntity> {
--- src/modules/drugs/drugs.controller.ts
+++ src/modules/drugs/drugs.controller.ts
@@ -1,31 +1,35 @@
 import { Controller, Get, Post, Body, BadGatewayException, ValidationPipe, Put, Delete } from '@nestjs/common';
 import { drugServices } from './drugs.service';
 import { createDTO, readDTO, updateDTO, deleteDTO } from './drugs.dto';
-import { ApiQuery } from '@nestjs/swagger';
+import { ApiQuery, ApiTags } from '@nestjs/swagger';
 
 @Controller('api')
 export class drugController {
 
     constructor (private readonly drugServices: drugServices) {}
 
+    @ApiTags('DRUGS')
     @ApiQuery({ type: createDTO})
     @Post('insert')
     createDrug(@Body(new ValidationPipe({ transform: true })) body: createDTO) {
       return this.drugServices.createDrug(body);
     }
 
+    @ApiTags('DRUGS')
     @Get('read')
     @ApiQuery({ type: readDTO})
     readDrug() {
         return this.drugServices.readDrug();
     }
 
+    @ApiTags('DRUGS')
     @Put('update')
     @ApiQuery({ type: updateDTO})
     updateDrug(@Body(new ValidationPipe({ transform: true })) body: updateDTO) {
         return this.drugServices.updateDrug(body);
     }
 
+    @ApiTags('DRUGS')
     @Delete('delete')
     @ApiQuery({ type: deleteDTO})
     deleteDrug(@Body(new ValidationPipe({ transform: true })) body: deleteDTO) {
