941db6a35a1b590a2af3c6f814f620cf0ea886d0
Julio Kozarewicz <julioerk@outlook.com>
2024-07-26 13:39:27 -0300

    [ DOCS ] ( development ) Add more info

--- src/modules/accounts/accounts.controller.ts
+++ src/modules/accounts/accounts.controller.ts
@@ -13,43 +13,63 @@ export class UserController {
     ) {}
 
     @Post('signup')
+    @ApiBody({ type: UserEntityDTO })
     @ApiOperation({
         summary: 'Create a new user',
         description: 'Registers a new user in the system with the provided details.'
     })
-    @ApiBody({ type: UserEntityDTO })
     @ApiResponse({ status: 201, description: 'User successfully created.' })
     @ApiResponse({ status: 400, description: 'Invalid input.' })
     async createUser(@Body(new ValidationPipe({ transform: true })) createUserDto: UserEntityDTO): Promise<any> {
         return await this.userService.createUser(createUserDto);
     }
 
-    @ApiBody({ type: resendUserDTO})
     @Post('resend-verify-email')
+    @ApiBody({ type: resendUserDTO})
+    @ApiOperation({
+        summary: 'Resend Verification Email',
+        description: 'Resends a verification email to the user based on the provided email address.'
+      })
     async resendVerifyEmailCode(@Body(new ValidationPipe({ transform: true })) resendEmailDto: resendUserDTO) {
         return await this.userService.resendVerifyEmailCode(resendEmailDto);
     }
   
-    @ApiBody({ type: CodeAccountActivateDTO})
     @Post('verify-email')
+    @ApiBody({ type: CodeAccountActivateDTO})
+    @ApiOperation({
+        summary: 'Verify Email',
+        description: 'Verifies the user’s email address based on the provided code.'
+    })
     async verifyEmailCode (@Body(new ValidationPipe({ transform: true })) activateDTO: CodeAccountActivateDTO) { 
         return await this.userService.verifyEmailCode(activateDTO);
     }
 
-    @ApiBody({ type: changePasswordLinkDTO})
     @Post('change-password-link')
+    @ApiBody({ type: changePasswordLinkDTO})
+    @ApiOperation({
+        summary: 'Request Password Change Link',
+        description: 'Generates a password change link and sends it to the user’s email address.'
+    })
     async changePasswordLink(@Body(new ValidationPipe({ transform: true })) changePasswordLinkDTO: changePasswordLinkDTO) { 
         return await this.userService.changePasswordLink(changePasswordLinkDTO);
     }
 
-    @ApiBody({ type: changePasswordDTO})
     @Post('change-password')
+    @ApiBody({ type: changePasswordDTO})
+    @ApiOperation({
+        summary: 'Change User Password',
+        description: 'Allows the user to change their password by providing the code.'
+    })
     async changePassword(@Body(new ValidationPipe({ transform: true })) changePasswordDTO: changePasswordDTO) { 
         return await this.userService.changePassword(changePasswordDTO);
     }
 
-    @ApiBody({ type: LoginDTO})
     @Post('login')
+    @ApiBody({ type: LoginDTO})
+    @ApiOperation({
+        summary: 'User Login',
+        description: 'Allows the user to log in by providing their username and password.'
+      })
     async login(@Body(new ValidationPipe({ transform: true })) loginDto: LoginDTO) {
         return this.userService.login(loginDto);
     }
