729639f0d1c7e13720a88a88d7b6b939c8df8d81
Julio Kozarewicz <julioerk@outlook.com>
2024-08-09 14:17:55 -0300

    [ AUTH ] ( module ) Change db connection.

--- src/app.module.ts
+++ src/app.module.ts
@@ -26,7 +26,7 @@ import { ThrottlerModule } from '@nestjs/throttler';
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
+      synchronize: Boolean(process.env.DEBUG),
     } as TypeOrmModuleOptions)
 
   ],
